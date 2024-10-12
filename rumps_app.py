7122b02d866f5d4de0d95888eb1be75c2ea9c0e8
tmaruy <marui1009@yahoo.co.jp>
2024-10-12 16:54:10 +0900

    simplify

--- rumps_app.py
+++ rumps_app.py
@@ -8,47 +8,52 @@ import subprocess
 
 with open("key.yaml") as f: config = yaml.safe_load(f)
 
-
 def show_alert(title, message):
     script = f'display dialog "{message}" with title "{title}"'
     subprocess.run(['osascript', '-e', script])
-
-def userclick(app, menuitem):
-    db = config["tables"][menuitem.title]
-    key = config["api_key"]
-    props_db = get_notion_db(db, key)
-
-    n = 0
-    for entry in props_db:
-        doi = entry["doi"]["url"]
-        title = entry["Title"]["title"]
-        if (doi is not None) and len(title) == 0:
-            paper_info = get_paper_info(doi)
-            push_notion_db(paper_info, db, key)
-            n += 1
-    if n > 0:
-        show_alert("Done!", f"Filled information about {n} entries") 
-    else:
-        show_alert("Done!", "No update")
-
 
 class NotionCitationApp(App):
 
     def __init__(self):
         super(NotionCitationApp, self).__init__("NotionCitationApp")
-        self.menu=[
-            MenuItem(k, callback=self.set_table(key=k, id=v)) for k,v in config["tables"].items()
+        self.menu = [
+            [
+                MenuItem("Settings"), [
+                    MenuItem("API key", self.add_api_key), 
+                    MenuItem("Add database", )
+                ]
+            ], 
+            None,
+            [
+                MenuItem("Database"), [
+                    MenuItem(k, callback=self.userclick) for k,v in config["database"].items()
+                ]
+            ],
+            None
         ]
-        self.table_key = list(config["tables"].keys())[0]
-        self.table_id = config["tables"][self.table_key]
+        self.table_key = list(config["database"].keys())[0]
+        self.table_id = config["database"][self.table_key]
         self.icon = "static/logo.png"
     
-    for k,v in config["tables"].items():
-        userclick = clicked(k)(userclick)
+    def add_api_key(self, menuitem):
+        response = rumps.Window('Add your API key', dimensions=(200, 20)).run()
 
-    def set_table(self, key, id):
-        self.table_key = key
-        self.table_id = id
+    def userclick(self, menuitem):
+        db = config["database"][menuitem.title]["id"]
+        key = config["api_key"]
+        props_db = get_notion_db(db, key)
+        n = 0
+        for entry in props_db:
+            doi = entry["doi"]["url"]
+            title = entry["Title"]["title"]
+            if (doi is not None) and len(title) == 0:
+                paper_info = get_paper_info(doi)
+                push_notion_db(paper_info, db, key)
+                n += 1
+        if n > 0:
+            show_alert("Done!", f"Filled information about {n} entries") 
+        else:
+            show_alert("Done!", "No update")
  
 if __name__ == "__main__":
     app = NotionCitationApp()
